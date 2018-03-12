221365bfdf528fbc6f00de27d88ad6e63f77833d
kozakana <uncle6927@gmail.com>
2018-03-12 11:50:02 +0000

    Add items/show

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:show, :new, :create]
-
-  resources :items,  only: [:new]
+  resources :users,      only: [:show, :new, :create]
+  resources :items,      only: [:show, :new]
   resources :ownerships, only: [:create, :destroy]
 end
